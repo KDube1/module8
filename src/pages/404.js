997da030e75b50969609a9f7f08a1b2bc347e39d
KDube1 <krishna.dube1@gmail.com>
2020-07-16 16:09:50 -0400

    minor tweaks

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import Typography from "@material-ui/core/Typography";
 
 function NothingHere() {
     return (
-        <Typography>nothing </Typography>
+        <Typography>This page has not been built yet! Start from the home page!</Typography>
     )
 }
 
